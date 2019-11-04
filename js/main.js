4b85d5a2e0bf1be9faa8b72e8274706f4af9af6b
guozheng <a2604155202@gmail.com>
2019-11-04 18:12:25 +0800

    基础修改

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["我是开心", "我是一个很厉害的女孩", "爱生活，爱做饭"],
+        strings: ["我是开心仙女", "我是一个很厉害的女孩", "爱生活，爱做饭"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
